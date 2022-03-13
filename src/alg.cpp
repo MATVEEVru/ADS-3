68b3b712a28c5430bc77163af64fc0d4afa8146e
Матвеев Артем <99340742+MATVEEVru@users.noreply.github.com>
2022-03-13 23:24:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,11 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+   int m = 0;
+     for (int j = 0; j <= size; j++) {
+       if((arr+j)==value);
+       m+=1
+       }
+  return m;
   return 0; // если ничего не найдено
 }
